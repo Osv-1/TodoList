cc681b2f4eb1672589353a6374ed3c287d725fe3
Takeo <oswaldotakeo1@gmail.com>
2022-02-09 16:21:52 -0300

    feat: add timePicker

--- app/src/main/java/com/example/todolist/ui/AddTaskActivity.kt
+++ app/src/main/java/com/example/todolist/ui/AddTaskActivity.kt
@@ -6,9 +6,13 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.todolist.databinding.ActivityAddTaskBinding
+import com.example.todolist.datasource.TaskDataSource
 import com.example.todolist.extensions.format
 import com.example.todolist.extensions.text
+import com.example.todolist.model.Task
 import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import java.util.*
 
 
@@ -30,12 +34,48 @@ class AddTaskActivity : AppCompatActivity() {
             datePicker.addOnPositiveButtonClickListener {
                 val timeZone = TimeZone.getDefault()
                 val offset = timeZone.getOffset(Date().time) * -1
+
+                //preenchimento da data
                 binding.tilDate.text = Date(it + offset).format()
 
             }
             datePicker.show(supportFragmentManager, "DATE_PICKER_TAG")
         }
+        binding.tilHour.editText?.setOnClickListener {
+            val timePicker = MaterialTimePicker
+                .Builder()
+                .setTimeFormat(TimeFormat.CLOCK_24H)
+                .build()
 
+            //preenchimento da hora
+            timePicker.addOnPositiveButtonClickListener {
+               val minute = if (timePicker.minute in 0..9) "0${timePicker.minute}" else timePicker.minute
+
+               val hour = if (timePicker.hour in 0..9) "0${timePicker.hour}" else timePicker.hour
+
+                binding.tilHour.text = "${hour}: ${minute}"
+
+            }
+            timePicker.show(supportFragmentManager, null)
+        }
+
+        //botao cancelar
+        binding.btnCancel.setOnClickListener {
+            finish()
+        }
+
+        //botao de adicionar
+        binding.btnNewTask.setOnClickListener {
+            val task = Task(
+                title = binding.tilTitle.text,
+                date = binding.tilDate.text,
+                hour = binding.tilHour.text
+            )
+            TaskDataSource.insertTask(task)
+            Log.e(TAG, "insertListerners: "+ TaskDataSource.getList() )
+
+        }
+        
     }
 
 }
