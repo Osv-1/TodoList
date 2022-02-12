f733076f7da892b7b017104ac391d40a2eb71bd2
Takeo <oswaldotakeo1@gmail.com>
2022-02-12 10:49:57 -0300

    feat: add empty_states and removed startActivityForResult

--- app/src/main/java/com/example/todolist/ui/AddTaskActivity.kt
+++ app/src/main/java/com/example/todolist/ui/AddTaskActivity.kt
@@ -1,5 +1,6 @@
 package com.example.todolist.ui
 
+import android.app.Activity
 import android.content.ContentValues.TAG
 import android.os.Bundle
 
@@ -22,61 +23,69 @@ class AddTaskActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityAddTaskBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        insertListerners()
 
+        if (intent.hasExtra(TASK_ID)) {
+            val taskId = intent.getIntExtra(TASK_ID, 0)
+            TaskDataSource.findById(taskId)?.let {
+                binding.tilTitle.text = it.title
+                binding.tilDate.text = it.date
+                binding.tilHour.text = it.hour
+            }
+        }
+
+        insertListeners()
     }
 
-    private fun insertListerners() {
+    private fun insertListeners() {
         binding.tilDate.editText?.setOnClickListener {
             val datePicker = MaterialDatePicker.Builder.datePicker().build()
+
             datePicker.addOnPositiveButtonClickListener {
                 val timeZone = TimeZone.getDefault()
                 val offset = timeZone.getOffset(Date().time) * -1
-
-                //preenchimento da data
                 binding.tilDate.text = Date(it + offset).format()
-
             }
             datePicker.show(supportFragmentManager, "DATE_PICKER_TAG")
         }
+
         binding.tilHour.editText?.setOnClickListener {
-            val timePicker = MaterialTimePicker
-                .Builder()
+            val timePicker = MaterialTimePicker.Builder()
                 .setTimeFormat(TimeFormat.CLOCK_24H)
                 .build()
 
-            //preenchimento da hora
             timePicker.addOnPositiveButtonClickListener {
-               val minute = if (timePicker.minute in 0..9) "0${timePicker.minute}" else timePicker.minute
+                val minute = if (timePicker.minute in 0..9) "0${timePicker.minute}" else timePicker.minute
+                val hour = if (timePicker.hour in 0..9) "0${timePicker.hour}" else timePicker.hour
 
-               val hour = if (timePicker.hour in 0..9) "0${timePicker.hour}" else timePicker.hour
-
-                binding.tilHour.text = "${hour}: ${minute}"
-
+                binding.tilHour.text = "$hour:$minute"
             }
+
             timePicker.show(supportFragmentManager, null)
         }
 
-        //botao cancelar
         binding.btnCancel.setOnClickListener {
             finish()
         }
 
-        //botao de adicionar
         binding.btnNewTask.setOnClickListener {
             val task = Task(
                 title = binding.tilTitle.text,
                 date = binding.tilDate.text,
-                hour = binding.tilHour.text
+                hour = binding.tilHour.text,
+                id = intent.getIntExtra(TASK_ID, 0)
             )
             TaskDataSource.insertTask(task)
-            Log.e(TAG, "insertListerners: "+ TaskDataSource.getList() )
+            setResult(Activity.RESULT_OK)
             finish()
-
         }
-        
+    }
+
+
+    companion object {
+        const val TASK_ID = "task_id"
     }
 
 }
--- app/src/main/java/com/example/todolist/ui/MainActivity.kt
+++ app/src/main/java/com/example/todolist/ui/MainActivity.kt
@@ -1,30 +1,58 @@
 package com.example.todolist.ui
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.todolist.databinding.ActivityMainBinding
+import com.example.todolist.datasource.TaskDataSource
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var  binding: ActivityMainBinding
-    private val adapter by lazy {TaskListAdapter()}
+    private lateinit var binding: ActivityMainBinding
+    private val adapter by lazy { TaskListAdapter() }
+
+
+    private val register =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) updateList()
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-         binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.rvTasks.adapter = adapter
-
+        updateList()
 
         insertListeners()
     }
 
     private fun insertListeners() {
-        binding.fab.setOnClickListener{
-            startActivity(Intent(this, AddTaskActivity::class.java))
+        binding.fab.setOnClickListener {
+            register.launch(Intent(this, AddTaskActivity::class.java))
+        }
+
+        adapter.listenerEdit = {
+            val intent = Intent(this, AddTaskActivity::class.java)
+            intent.putExtra(AddTaskActivity.TASK_ID, it.id)
+            register.launch(intent)
+        }
+
+        adapter.listenerDelete = {
+            TaskDataSource.deleteTask(it)
+            updateList()
         }
     }
 
+    private fun updateList() {
+        val list = TaskDataSource.getList()
+        binding.includeEmpty.emptyState.visibility = if (list.isEmpty()) View.VISIBLE
+        else View.GONE
+
+        adapter.submitList(list)
+    }
 }
